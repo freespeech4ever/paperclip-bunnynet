2486635f2e5a587a5fd5f805c84f7497e54a3e89
free speech forever <trip@basher.me>
2024-11-27 23:05:38 -0500

    logs

--- lib/paperclip/storage/bunnynet.rb
+++ lib/paperclip/storage/bunnynet.rb
@@ -17,31 +17,36 @@ module Paperclip
 
       def exists?(style_name = default_style)
         url = bunnynet_url(style_name)
+        puts "Checking if #{url} exists for style #{style_name}"
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
         request = Net::HTTP::Head.new(url.path)
         request["AccessKey"] = @bunnynet_api_key
         response = http.request(request)
+        puts "Response code: #{response.code}"
         response.code.to_i == 200
       end
 
       def public_exists?(style_name = default_style)
         url = URI.parse(public_url(style_name))
+        puts "Checking if #{url} public exists for style #{style_name}"
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
         request = Net::HTTP::Head.new(url.path)
         response = http.request(request)
+        puts "Response code: #{response.code}"
         response.code.to_i == 200
       end
 
       def copy_to_local_file(style, local_dest_path)
         url = bunnynet_url(style)
+        puts "Copying to local file #{url} for style #{style}"
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
         request = Net::HTTP::Get.new(url.path)
         request["AccessKey"] = @bunnynet_api_key
         response = http.request(request)
-        
+        puts "Response code: #{response.code}"
         if response.is_a?(Net::HTTPSuccess)
           File.open(local_dest_path, 'wb') do |file|
             file.write(response.body)
@@ -52,8 +57,10 @@ module Paperclip
       end
 
       def flush_writes
+        puts "Flushing writes"
         @queued_for_write.each do |style_name, file|
           url = bunnynet_url(style_name)
+          puts "Uploading #{url} for style #{style_name}"
           # check an environment variable, BUNNYNET_SKIP_IF_EXISTS, and if true, check exists? first.
           if ENV['BUNNYNET_SKIP_IF_EXISTS'] && public_exists?(style_name)
             puts "paperclip-bunnynet: Skipping upload for #{path(style_name)} because it already exists."
@@ -65,7 +72,7 @@ module Paperclip
           request["AccessKey"] = @bunnynet_api_key
           request.body = file.read
           response = http.request(request)
-          
+          puts "Response code: #{response.code}"
           unless response.is_a?(Net::HTTPSuccess)
             raise "Failed to upload file to Bunny.net: #{response.code} - #{response.message}"
           end
@@ -74,27 +81,29 @@ module Paperclip
       end
 
       def flush_deletes
-        @queued_for_delete.each do |path|
-          url = URI.parse("#{@bunnynet_storage_url}/#{@bunnynet_storage_zone}/#{path}")
-          http = Net::HTTP.new(url.host, url.port)
-          http.use_ssl = true
-          request = Net::HTTP::Delete.new(url.path)
-          request["AccessKey"] = @bunnynet_api_key
-          response = http.request(request)
-          
-          unless response.is_a?(Net::HTTPSuccess)
-            raise "Failed to delete file from Bunny.net: #{response.code} - #{response.message}"
-          end
-        end
+        #@queued_for_delete.each do |path|
+          #url = URI.parse("#{@bunnynet_storage_url}/#{@bunnynet_storage_zone}/#{path}")
+          #http = Net::HTTP.new(url.host, url.port)
+          #http.use_ssl = true
+          #request = Net::HTTP::Delete.new(url.path)
+          #request["AccessKey"] = @bunnynet_api_key
+         # response = http.request(request)
+         # 
+         # unless response.is_a?(Net::HTTPSuccess)
+         #   raise "Failed to delete file from Bunny.net: #{response.code} - #{response.message}"
+        #  end
+        #end
         @queued_for_delete = []
       end
 
       def bunnynet_url(style_name = default_style)
+        puts "Getting Bunny.net URL for #{path(style_name)} for style #{style_name}"
         path = path(style_name)
         URI.parse("#{@bunnynet_storage_url}/#{@bunnynet_storage_zone}/#{path}")
       end
 
       def public_url(style_name = default_style)
+        puts "Getting Bunny.net public URL for #{path(style_name)} for style #{style_name}"
         path = path(style_name)
         "#{@bunnynet_cdn_url}/#{path}"
       end
